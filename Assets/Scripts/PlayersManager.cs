925419f7605e2e2df64ca9bf6b2a8ee4c4fee3aa
rembab <rbabiarz@student.agh.edu.pl>
2025-04-12 13:41:13 +0200

    Random
    subject to change

---
+++ Assets/Scripts/PlayersManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayersManager : MonoBehaviour
+{
+    List<int> players = new List<int>();
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
